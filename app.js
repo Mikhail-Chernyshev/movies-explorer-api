af1f49e77c5a23bfc5a763b1a236e65120d2ddd7
Mikhail Chernyshev <newmishach@gmail.com>
2022-12-07 00:34:04 +0700

    addedRoutesToIndex.js

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 // require('dotenv').config();
 const express = require("express");
-const { PORT = 3001 } = process.env;
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const app = express();
 const auth = require("./middlewares/auth");
-const { createUser, login } = require("./controllers/users");
+// const { createUser, login } = require("./controllers/users");
 const { errors, celebrate, Joi } = require("celebrate");
 const errorServer = require("./middlewares/error");
-const routesUsers = require("./routes/users");
-const routesMovies = require("./routes/movies");
+// const routesUsers = require("./routes/users");
+// const routesMovies = require("./routes/movies");
+const apiRouter = require("./routes/index");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
+const { PORT = 3001, MONGO_URL = "mongodb://localhost:27017/bitfilmsdb" } =
+  process.env;
 
-mongoose.connect("mongodb://localhost:27017/bitfilmsdb", {
+mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
 });
 
@@ -22,35 +24,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(requestLogger);
 
-app.post(
-  "/signin",
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
-  }),
-  login
-);
-app.post(
-  "/signup",
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
-  }),
-  createUser
-);
+// app.post(
+//   "/signin",
+//   celebrate({
+//     body: Joi.object().keys({
+//       email: Joi.string().required().email(),
+//       password: Joi.string().required(),
+//     }),
+//   }),
+//   login
+// );
+// app.post(
+//   "/signup",
+//   celebrate({
+//     body: Joi.object().keys({
+//       name: Joi.string().required().min(2).max(30),
+//       email: Joi.string().required().email(),
+//       password: Joi.string().required(),
+//     }),
+//   }),
+//   createUser
+// );
 
-app.use(auth);
+// app.use(auth);
 
-app.use("/users", routesUsers);
-app.use("/movies", routesMovies);
-app.use("*", (req, res, next) => {
-  next(new NotFoundError("Маршрут не найден"));
-});
+// app.use("/users", routesUsers);
+// app.use("/movies", routesMovies);
+app.use("/", apiRouter);
+// app.use("*", (req, res, next) => {
+//   next(new NotFoundError("Маршрут не найден"));
+// });
 app.use(errorLogger);
 
 app.use(errors());
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+
+const { createUser, login } = require("../controllers/users");
+const auth = require("../middlewares/auth");
+
+router.post("/signup", createUser);
+
+router.post("/signin", login);
+
+router.use(auth);
+
+router.use("/users", require("./users"));
+
+router.use("/movies", require("./movies"));
+
+router.use("*", (req, res, next) => {
+  next(new NotFoundError("Маршрут не найден"));
+});
+
+module.exports = router;

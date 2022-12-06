cfa2ff3a3622dd069362c609dc70d47f6d3ec05e
Mikhail Chernyshev <newmishach@gmail.com>
2022-12-07 01:48:23 +0700

    addedHelmetAndLimiter

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const app = express();
-const auth = require("./middlewares/auth");
+const helmet = require("helmet");
 const { errors, celebrate, Joi } = require("celebrate");
 const errorServer = require("./middlewares/error");
 const apiRouter = require("./routes/index");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 const { PORT = 3001, MONGO_URL = "mongodb://localhost:27017/bitfilmsdb" } =
   process.env;
+const limiter = require("./utils/limiter");
 
 mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
@@ -19,6 +20,8 @@ mongoose.connect(MONGO_URL, {
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(helmet());
+app.use(limiter);
 app.use(requestLogger);
 
 app.use("/", apiRouter);

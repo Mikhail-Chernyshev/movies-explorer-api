a9ea738a273956d0cf027abec3355cf0a7b28b48
Mikhail Chernyshev <newmishach@gmail.com>
2022-12-06 19:16:11 +0700

    fixrequiredname

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ const userSchema = new Schema(
       validate: { validator: (v) => validator.isEmail(v) },
     },
     password: { type: String, required: true, select: false },
-    name: { type: String, required: false, minlength: 2, maxlength: 30 },
+    name: { type: String, required: true, minlength: 2, maxlength: 30 },
   },
   { versionKey: false }
 );

4f5a81c3f1895d7696d9f794a57e06091e137b9e
Mikhail Chernyshev <newmishach@gmail.com>
2022-12-29 00:34:00 +0700

    noCors

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const app = express();
 const helmet = require("helmet");
-const { errors} = require("celebrate");
+const { errors } = require("celebrate");
+const cors = require("cors");
 const errorServer = require("./middlewares/error");
 const apiRouter = require("./routes/index");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 const { PORT = 3001, MONGO_URL = "mongodb://localhost:27017/bitfilmsdb" } =
   process.env;
 const limiter = require("./utils/limiter");
-
+const corsCan = { Origin: "http://localhost:3000" };
 
 mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
 });
+app.use(cors(corsCan));
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
